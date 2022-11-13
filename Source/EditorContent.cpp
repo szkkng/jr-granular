0ed31e02b8a13ad26f9e1297a9123a54a01cc558
szkkng <zukkesukke23@gmail.com>
2022-11-13 16:16:56 +0900

    Set default argument for UndoManager to nullptr

--- Source/EditorContent.cpp
+++ Source/EditorContent.cpp
@@ -24,13 +24,13 @@
 
 EditorContent::EditorContent (juce::AudioProcessorValueTreeState& apvts,
                               juce::UndoManager& um)
-    : intvDial  (*apvts.getParameter (ParamIDs::interval),  um),
-      pitchDial (*apvts.getParameter (ParamIDs::pitch),     um),
-      sizeDial  (*apvts.getParameter (ParamIDs::grainSize), um),
-      posDial   (*apvts.getParameter (ParamIDs::grainPos),  um),
-      widthDial (*apvts.getParameter (ParamIDs::width),     um),
-      mixDial   (*apvts.getParameter (ParamIDs::mix),       um),
-      gainDial  (*apvts.getParameter (ParamIDs::gain),      um)
+    : intvDial  (*apvts.getParameter (ParamIDs::interval),  &um),
+      pitchDial (*apvts.getParameter (ParamIDs::pitch),     &um),
+      sizeDial  (*apvts.getParameter (ParamIDs::grainSize), &um),
+      posDial   (*apvts.getParameter (ParamIDs::grainPos),  &um),
+      widthDial (*apvts.getParameter (ParamIDs::width),     &um),
+      mixDial   (*apvts.getParameter (ParamIDs::mix),       &um),
+      gainDial  (*apvts.getParameter (ParamIDs::gain),      &um)
 {
     setWantsKeyboardFocus (true);
 
