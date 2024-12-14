e1012f8cea83f269a5a9b2597c797078d9d44a9c
Kengo Suzuki <git@kengo.dev>
2024-12-14 13:33:04 +0900

    Use keychain-profile for notarization

--- cmake/Notarization.cmake
+++ cmake/Notarization.cmake
@@ -1,7 +1,6 @@
 execute_process(
     COMMAND
-        xcrun notarytool submit --wait --apple-id "$ENV{APPLE_DEVELOPER_ID}" --team-id "$ENV{APPLE_TEAM_ID}" --password
-        "$ENV{APPLE_PASSWORD}" "${CPACK_PACKAGE_FILES}"
+        xcrun notarytool submit "${CPACK_PACKAGE_FILES}" --keychain-profile "notarytool-password" --wait
     TIMEOUT 120
     COMMAND_ECHO STDOUT
     COMMAND_ERROR_IS_FATAL ANY
