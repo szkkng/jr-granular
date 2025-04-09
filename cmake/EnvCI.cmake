31c51817e4ce53201baa080d5e5eb55af06e9ce5
szkkng <hi@kengo.dev>
2025-04-09 11:26:26 +0900

    Clean up

--- cmake/EnvCI.cmake
+++ cmake/EnvCI.cmake
@@ -2,7 +2,7 @@ if(DEFINED ENV{CI})
     set(output_env_file "${CMAKE_SOURCE_DIR}/.env.ci")
     message(STATUS "Writing ENV file for CI: ${output_env_file}")
 
-    set(required_vars PRODUCT_NAME PACKAGE_DIRECTORY PACKAGE_FILE_NAME PROJECT_VERSION)
+    set(required_vars PACKAGE_DIRECTORY PACKAGE_FILE_NAME)
     foreach(var IN LISTS required_vars)
         if(NOT DEFINED ${var})
             message(FATAL_ERROR "Required environment variable '${var}' is not defined.")
@@ -17,12 +17,7 @@ if(DEFINED ENV{CI})
     endif()
 
     set(PACKAGE_FILE_PATH "${PACKAGE_DIRECTORY}/${PACKAGE_FILE_NAME}${PACKAGE_FILE_EXT}")
-    set(env_vars
-        "PRODUCT_NAME=${PROJECT_NAME}"
-        "PACKAGE_FILE_NAME=${PACKAGE_FILE_NAME}"
-        "PACKAGE_FILE_PATH=${PACKAGE_FILE_PATH}"
-        "VERSION=${PROJECT_VERSION}"
-    )
+    set(env_vars "PACKAGE_FILE_NAME=${PACKAGE_FILE_NAME}" "PACKAGE_FILE_PATH=${PACKAGE_FILE_PATH}")
 
     file(WRITE "${output_env_file}" "")
     foreach(var IN LISTS env_vars)
